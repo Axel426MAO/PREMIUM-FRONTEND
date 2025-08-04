c542d24034f68a0f4e7025fd469e25aef5bda7e5
Your Name <you@example.com>
2025-08-04 18:12:42 -0300

    a

--- src/app/admin/books/form/page.tsx
+++ src/app/admin/books/form/page.tsx
@@ -28,7 +28,7 @@ import {
 } from "../services/api";
 
 // --- TIPOS E CONSTANTES INTERNAS ---
-const API_DOMAIN = "http://212.85.14.247/:4000";
+const API_DOMAIN = "http://212.85.14.247:4000/";
 
 interface UnifiedPreview {
   key: string;
